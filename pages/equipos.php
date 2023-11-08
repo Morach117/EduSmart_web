ef7e2d00d3714c27825233d1692bd93ef38f7e17
JESUS <garciacaballerojesus285@gmail.com>
2023-11-08 12:15:36 -0600

    cambios en equipos.php para agregar un datatable pero no sé que chingados lleva la tabla

--- pages/equipos.php
+++ pages/equipos.php
@@ -1,32 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Equipos</title>
 </head>
+
 <body>
     <div class="page-wrapper">
         <div class="page-body">
             <div class="container-xl">
-                <div class="card">
-                    <div class="text-end p-5">
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modal-elegir-alumnos">
-                            Administrar equipos
-                        </button>
-                    </div>
-                    <div class="table-responsive">
-                        <table id="equipos-table" class="table table-center card-table">
-                            <thead>
-                                <tr>
-                                    <th>NO</th>
-                                    <th>Nombre del equipo</th>
-                                    <th>Integrantes</th>
-                                    <th>Detalles</th>
-                                </tr>
-                            </thead>
-                            <!-- Cuerpo de la tabla -->
-                        </table>
+                <div class="container">
+                    <div class="card">
+                        <div class="text-end pt-3 pe-5">
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#modal-elegir-alumnos">
+                                Administrar equipos
+                            </button>
+                        </div>
+                        <div class="card-body">
+                            <fieldset class="form-fieldset">
+                                <div class="col">
+                                    <h2 class="page-title">
+                                        Modulo de selección de equipos
+                                    </h2>
+                                    <h2 class="page-pretitle">
+                                        ingresa el nombre de la unidad temática
+                                        que deseas modificar
+                                    </h2>
+                                    <hr class="m-0" />
+                                    <div class="table-responsive">
+                                        <table id="equipos-table" class="table table-striped table-hover"
+                                            style="width:100%">
+                                            <thead>
+                                                <tr>
+                                                    <th>NO</th>
+                                                    <th>Nombre del equipo</th>
+                                                    <th>Integrantes</th>
+                                                    <th>Detalles</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <?php
+                                                $selAlumno = $conn->query("SELECT * FROM alumnos ORDER BY id_alumno");
+                                                if ($selAlumno->rowCount() > 0) {
+                                                    while ($selAlumnoRow = $selAlumno->fetch(PDO::FETCH_ASSOC)) {
+                                                        ?>
+                                                        <tr>
+                                                            <td>
+                                                                <?php echo $selAlumnoRow['matricula'] ?>
+                                                            </td>
+                                                            <td>
+                                                                <?php echo $selAlumnoRow['nombre'] ?>
+                                                            </td>
+                                                            <td>
+                                                                <?php echo $selAlumnoRow['nombre'] ?>
+                                                            </td>
+                                                            <td>
+                                                                <?php echo $selAlumnoRow['nombre'] ?>
+                                                            </td>
+                                                        </tr>
+                                                        <?php
+                                                    }
+                                                } else {
+                                                    echo "No hay alumnos registrados.";
+                                                }
+                                                ?>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -84,11 +130,11 @@
                                 <!-- Cuerpo de la tabla de alumnos -->
                             </tbody>
                         </table>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary">Guardar</button>
+                </div>
                 </form>
             </div>
         </div>
@@ -97,7 +143,7 @@
     <script>
         $(document).ready(function () {
             $('#equipos-table').DataTable();
-            
+
             var dataTable = $('#alumnos-table').DataTable({
                 stateSave: true,
                 "processing": true,
@@ -135,21 +181,21 @@
                     }
                 }
             });
-            
+
             // Captura el evento de envío del formulario
             $('#equipo-form').on('submit', function (e) {
                 e.preventDefault();
-                
+
                 // Recopila los checkboxes marcados en un array
                 var alumnosSeleccionados = [];
                 $('input[name="alumnos_seleccionados[]"]:checked').each(function () {
                     alumnosSeleccionados.push($(this).val());
                 });
-                
+
                 // Recopila otros datos del formulario si es necesario
                 var nombreEquipo = $('#nombre-equipo').val();
                 var integrantesEquipo = $('#integrantes').val();
-                
+
                 // Realiza una solicitud AJAX para guardar los datos
                 $.ajax({
                     type: 'POST',
@@ -184,4 +230,5 @@
         });
     </script>
 </body>
+
 </html>
