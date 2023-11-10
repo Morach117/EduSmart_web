a3f87c092c84d9aab3c3d0c2edb72332fa9ee3e4
Morach117 <morach117@gmail.com>
2023-11-10 01:34:31 -0600

    no se

--- examen2.php
+++ examen2.php
@@ -1,3 +1,48 @@
+<?php
+
+session_start(); // inicia la sesion
+
+if (!isset($_SESSION['admin']['adminnakalogin']) == true) {
+    header("location:index.php"); // si el usuario no inicio sesion, lo redirige a la pagina de login
+}
+
+include('conn.php'); // Incluir archivo de conexión a la base de datos
+include('includes/navbar.php'); // Incluir archivo de barra de navegación
+
+
+// Obtener el ID del examen de la URL
+$id_examen = isset($_GET['id']) ? $_GET['id'] : null;
+
+// Verificar si se proporcionó un ID de examen
+if ($id_examen) {
+    // Consultar la base de datos para obtener los detalles del examen
+    // (Asumiendo que tienes una conexión a la base de datos llamada $conn)
+    $stmt = $conn->prepare("SELECT * FROM examenes WHERE id_examen = :id_examen");
+    $stmt->bindParam(':id_examen', $id_examen, PDO::PARAM_INT);
+    $stmt->execute();
+    
+    // Obtener los datos del examen
+    $examen = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // Verificar si se encontraron datos
+    if ($examen) {
+        // Mostrar los detalles del examen en los campos del formulario
+        $titulo = $examen['titulo'];
+        $pregunta1 = $examen['pregunta1'];
+        $pregunta2 = $examen['pregunta2'];
+        $pregunta3 = $examen['pregunta3'];
+    } else {
+        // No se encontró el examen con el ID proporcionado
+        echo "Examen no encontrado";
+        exit;
+    }
+} else {
+    // No se proporcionó un ID de examen
+    echo "ID de examen no proporcionado";
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,7 +64,7 @@
                                 <fieldset class="form-fieldset m-3">
                                     <div class="form-floating mb-3">
                                         <input type="text" class="form-control" name="formId1" id="formId1"
-                                            placeholder="">
+                                            placeholder="" value="<?php echo $titulo; ?>">
                                         <label for="formId1">Titulo</label>
                                     </div>
                                     <div class="form-floating mb-3">
@@ -47,21 +92,21 @@
                                     <div class="mb-3">
                                         <div class="form-floating">
                                             <textarea class="form-control" placeholder="Leave a comment here"
-                                                id="floatingTextarea"></textarea>
+                                                id="floatingTextarea"><?php echo $pregunta1; ?></textarea>
                                             <label for="floatingTextarea">Pregunta 1</label>
                                         </div>
                                     </div>
                                     <div class="mb-3">
                                         <div class="form-floating">
                                             <textarea class="form-control" placeholder="Leave a comment here"
-                                                id="floatingTextarea"></textarea>
+                                                id="floatingTextarea"><?php echo $pregunta2; ?></textarea>
                                             <label for="floatingTextarea">Pregunta 2</label>
                                         </div>
                                     </div>
                                     <div class="mb-3">
                                         <div class="form-floating">
                                             <textarea class="form-control" placeholder="Leave a comment here"
-                                                id="floatingTextarea"></textarea>
+                                                id="floatingTextarea"><?php echo $pregunta3; ?></textarea>
                                             <label for="floatingTextarea">Pregunta 3</label>
                                         </div>
                                     </div>
