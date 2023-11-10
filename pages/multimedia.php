111b890f56c292e2611b18a498f599ffcd4b81a7
JESUS <garciacaballerojesus285@gmail.com>
2023-11-09 23:13:14 -0600

    Diseño de multimedia

--- pages/multimedia.php
+++ pages/multimedia.php
@@ -5,79 +5,112 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Administrar Contenido</title>
-    <!-- Incluye tus estilos personalizados si los tienes -->
-    <style>
-        body {
-            font-family: 'Arial', sans-serif;
-        }
-
-        .container {
-            margin-top: 50px;
-        }
-
-        .card {
-            margin-bottom: 20px;
-        }
-
-        .btn-add {
-            margin-top: 20px;
-        }
-
-        .table-container {
-            margin-top: 20px;
-        }
-    </style>
 </head>
 
 <body>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6">
-                <!-- Card para agregar temas -->
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Agregar Tema</h5>
-                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAgregarTema">Agregar Tema</button>
+    <div class="page-wrapper">
+        <div class="page-body">
+            <div class="cotainer-xl">
+                <div class="container">
+                    <div class="card">
+                        <div class="card-body">
+                            <fieldset class="form-fieldset">
+                                <div class="col">
+                                    <h2 class="page-title">
+                                        Modulo para la gestión de temas
+                                    </h2>
+                                    <h2 class="page-pretitle">
+                                    </h2>
+                                    <hr class="m-0" />
+                                    <div class="row g-2">
+                                        <div class="container my-3 card p-3 col">
+                                            <div class="row">
+                                                <h2 class="page-title col-8">
+                                                    Temas actuales
+                                                </h2>
+
+                                                <div class="col-4">
+                                                    <button class="btn btn-primary" data-bs-toggle="modal"
+                                                        data-bs-target="#modalAgregarTema">
+                                                        Agregar Tema
+                                                    </button>
+                                                </div>
+                                            </div>
+                                            <hr class="m-3" />
+                                            <div class="table-responsive">
+                                                <table id="temas-table"
+                                                    class="table table-striped table-hover text-center"
+                                                    style="width:100%">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">ID</th>
+                                                            <th scope="col">Tema</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr class="">
+                                                            <td>1</td>
+                                                            <td>Tema 1</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                        <div class="container my-3 card p-3 col">
+                                            <h2 class="page-title">
+                                                Modulo para la gestión de temas
+                                            </h2>
+                                            <hr class="m-3" />
+                                            <div class="text-end mb-3">
+                                                <button class="btn btn-primary" data-bs-toggle="modal"
+                                                    data-bs-target="#modalAgregarTema">
+                                                    Agregar Tema
+                                                </button>
+                                            </div>
+                                            <div class="table-responsive">
+                                                <table id="temas-table"
+                                                    class="table table-striped table-hover text-center"
+                                                    style="width:100%">
+                                                    <thead>
+                                                        <tr>
+                                                            <th scope="col">ID</th>
+                                                            <th scope="col">Tema</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        <tr class="">
+                                                            <td>1</td>
+                                                            <td>Tema 1</td>
+                                                        </tr>
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </fieldset>
+                        </div>
                     </div>
                 </div>
-
-                <!-- Tabla para mostrar temas -->
-                <div class="table-container">
-                    <h5>Temas Registrados</h5>
-                    <table class="table table-bordered">
-                        <!-- Aquí mostrarías dinámicamente los temas desde la base de datos -->
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Tema</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Tema 1</td>
-                            </tr>
-                            <!-- ... -->
-                        </tbody>
-                    </table>
-                </div>
             </div>
+        </div>
+    </div>
+    <!-- <div class="container">
+        <div class="row">
 
-            <!-- Columna para agregar subtemas -->
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Agregar Subtema</h5>
-                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAgregarSubtema">Agregar Subtema</button>
+                        <button class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modalAgregarSubtema">Agregar Subtema</button>
                     </div>
                 </div>
 
-                <!-- Tabla para mostrar subtemas -->
                 <div class="table-container">
                     <h5>Subtemas Registrados</h5>
                     <table class="table table-bordered">
-                        <!-- Aquí mostrarías dinámicamente los subtemas desde la base de datos -->
                         <thead>
                             <tr>
                                 <th>ID</th>
@@ -91,26 +124,23 @@
                                 <td>Tema 1</td>
                                 <td>Subtema 1</td>
                             </tr>
-                            <!-- ... -->
                         </tbody>
                     </table>
                 </div>
             </div>
 
-            <!-- Columna para agregar contenido relacionado -->
             <div class="col-md-6">
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-title">Agregar Contenido Relacionado</h5>
-                        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalAgregarContenido">Agregar Contenido</button>
+                        <button class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#modalAgregarContenido">Agregar Contenido</button>
                     </div>
                 </div>
 
-                <!-- Tabla para mostrar contenido relacionado -->
                 <div class="table-container">
                     <h5>Contenido Relacionado</h5>
                     <table class="table table-bordered">
-                        <!-- Aquí mostrarías dinámicamente el contenido desde la base de datos -->
                         <thead>
                             <tr>
                                 <th>ID</th>
@@ -124,7 +154,6 @@
                                 <td>Subtema 1</td>
                                 <td>Contenido relacionado 1</td>
                             </tr>
-                            <!-- ... -->
                         </tbody>
                     </table>
                 </div>
@@ -132,24 +161,49 @@
         </div>
     </div>
 
-    <!-- Modales para agregar -->
-    <!-- Modal para agregar tema -->
-    <div class="modal fade" id="modalAgregarTema" tabindex="-1" aria-labelledby="modalAgregarTemaLabel" aria-hidden="true">
-        <!-- Contenido del modal -->
+    <div class="modal fade" id="modalAgregarTema" tabindex="-1" aria-labelledby="modalAgregarTemaLabel"
+        aria-hidden="true">
     </div>
 
-    <!-- Modal para agregar subtema -->
-    <div class="modal fade" id="modalAgregarSubtema" tabindex="-1" aria-labelledby="modalAgregarSubtemaLabel" aria-hidden="true">
-        <!-- Contenido del modal -->
+    <div class="modal fade" id="modalAgregarSubtema" tabindex="-1" aria-labelledby="modalAgregarSubtemaLabel"
+        aria-hidden="true">
     </div>
 
-    <!-- Modal para agregar contenido -->
-    <div class="modal fade" id="modalAgregarContenido" tabindex="-1" aria-labelledby="modalAgregarContenidoLabel" aria-hidden="true">
-        <!-- Contenido del modal -->
+    <div class="modal fade" id="modalAgregarContenido" tabindex="-1" aria-labelledby="modalAgregarContenidoLabel"
+        aria-hidden="true">
+    </div> -->
+
+    <!-- Modal Body-->
+    <div class="modal fade " id="modalAgregarTema" tabindex="-1" role="dialog" aria-labelledby="modalTitleId"
+        aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="container-fluid">
+                        Add rows here
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+        </div>
     </div>
 
-    <!-- Incluye tus scripts si es necesario -->
 
+
+
+
+    <script>
+        $(document).ready(function () {
+            $('#temas-table').DataTable();
+        });
+    </script>
 </body>
 
 </html>
